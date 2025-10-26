fe735444c020d10e70e61e13058049f5de267475
esquilo come rato <julialorentz111@gmail.com>
2025-10-26 13:01:40 -0300

    kkkkkkkkk
    KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK

---
+++ penis2.py
@@ -0,0 +1,25 @@
+import time
+
+numero = input ("Escolha um numero de 1 a 10: ")
+pi = float(input ("Qual os 3 primeiros numeros de valor de PI? "))
+if pi == 3.14:
+    print ("boa intelijentao")
+else:
+    float(input ("burro tenta de novo, qual os 3 primeiros numeros de valor de PI?"))
+sup = input ("sup fadinha, de cura ou engage? ")
+if sup == "fadinha":
+    print ("KKKKKKK boa mlk")
+elif sup == "cura" or sup == "de cura":
+    print ("virgem do caralho")
+else:
+    print ("primeiramente que voce é prata! que sup de engage oque")
+filosofia = input ("o ser humano nasce bom?")
+if filosofia == "sim":
+    print ("hmmm... boa resposta")
+else:
+    print ("isso mesmo!")
+piscada = print ("agora fecha os olhos e conta ate 10!")
+
+time.sleep(5)
+#import time + time.sleep(numero de segundos que voce quer esperar aqui dentro dos parenteses)
+print ("agora vai tomar no cu")
